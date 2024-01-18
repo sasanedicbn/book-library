a68398cfb268e3e31be6f1580e279ef6d10f4e57
Sasa Nedic <svetasmak0@gmail.com>
2024-01-19 00:05:33 +0100

    Create classDom with showForm and hiddenForm

--- book-library/script.js
+++ book-library/script.js
@@ -1,3 +1,12 @@
 const btnNewBook = document.querySelector(".button__new__book");
 const form = document.querySelector(".formado");
 const closeForm = document.querySelector("svg");
+
+class updateDom {
+  showForm(element, selector) {
+    element.classList.add(selector);
+  }
+  hiddenForm(element, selector) {
+    element.classList.remove(selector);
+  }
+}
