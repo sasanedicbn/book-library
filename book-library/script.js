27407d445ef900947f148c157bc79bff53d1ea1a
Sasa Nedic <svetasmak0@gmail.com>
2024-01-19 23:17:30 +0100

    Create moveHeaderup inside updateDom class and change css code

--- book-library/script.js
+++ book-library/script.js
@@ -1,6 +1,8 @@
 const btnNewBook = document.querySelector(".button__new__book");
 const form = document.querySelector(".formado");
 const closeForm = document.querySelector("svg");
+const btnSubmitData = document.querySelector(".submit");
+const title = document.querySelector(".text__div");
 
 class updateDom {
   showForm(element, selector) {
@@ -9,11 +11,17 @@ class updateDom {
   hiddenForm(element, selector) {
     element.classList.remove(selector);
   }
+  moveHeaderup(element, selector) {
+    element.classList.add(selector);
+  }
 }
+
 const DOM = new updateDom();
+
 btnNewBook.addEventListener("click", function () {
   DOM.showForm(form, "show");
 });
+
 closeForm.addEventListener("click", function () {
   DOM.hiddenForm(form, "show");
 });
