802aed1e5d9769c6356f29761172470d194443b4
Sasa Nedic <svetasmak0@gmail.com>
2024-01-24 22:53:09 +0100

    Add classes in input

--- book-library/script.js
+++ book-library/script.js
@@ -28,11 +28,11 @@ class updateDom {
         book.isEditing
           ? `<form type="submit">
           <span class='title'>Title</span>
-          <input type='text' class='title' value='${book.title}'/>
+          <input type='text' class='inputTitle' value='${book.title}'/>
           <span>Author</span>
-          <input type='text' class='author' value='${book.author}'/>
+          <input type='text' class='inputAuthor' value='${book.author}'/>
           <span>Numbers of pages:</span>
-          <input type='text' class='numOfPages' value='${book.numPages}'/>
+          <input type='text' class='inputNumOfPages' value='${book.numPages}'/>
           <div class='btns'>
             <button class="toggleRead">${
               book.read ? "Read" : "Not Read"
