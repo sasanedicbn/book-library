21e6af9cb3f411c56d393def1ff409563fcbde04
Sasa Nedic <svetasmak0@gmail.com>
2024-01-25 18:25:03 +0100

    Change in code

--- book-library/script.js
+++ book-library/script.js
@@ -144,18 +144,7 @@ btnSubmitData.addEventListener("click", function (event) {
   DOM.showBook(data.books);
 });
 
-function changesInBook() {
-  function handleDeleteClick(event) {
-    const deleteButton = event.target.closest(".delete");
-    if (deleteButton) {
-      const currentBook = deleteButton.closest(".book");
-      const id = currentBook.dataset.id;
 
-      data.deleteBook(id);
-      currentBook.remove();
-      DOM.showBook(data.books);
-    }
-  }
 
   function handleToggleReadClick(event) {
     const toggleRead = event.target.closest(".toggleRead");
